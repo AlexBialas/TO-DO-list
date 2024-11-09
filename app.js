73a8522d12165e5aea02c1b006fe5f2697bae964
dci-student <aleksandra.bialas@dci-student.org>
2024-11-09 18:18:56 +0100

    commit

--- app.js
+++ app.js
@@ -7,20 +7,50 @@ function addTask() {
   } else {
     let li = document.createElement("li");
     li.innerHTML = inputBox.value;
-    listContainer.appendChild(li);
 
     let span = document.createElement("span");
     span.innerHTML = "\u00d7";
     li.appendChild(span);
 
+    listContainer.appendChild(li);
+
     inputBox.value = "";
+    saveData();
   }
 }
 
 listContainer.addEventListener("click", function (e) {
   if (e.target.tagName === "LI") {
     e.target.classList.toggle("checked");
+    saveData();
   } else if (e.target.tagName === "SPAN") {
     e.target.parentElement.remove();
+    saveData();
   }
 });
+
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showList() {
+  // Tu również dodajemy obsługę dla zdarzeń click dla elementów przywracanych z localStorage
+  const data = localStorage.getItem("data");
+  if (data) {
+    listContainer.innerHTML = data;
+    // Dodajemy event listener dla przywróconych elementów
+    Array.from(listContainer.getElementsByTagName("li")).forEach((li) => {
+      li.addEventListener("click", function (e) {
+        if (e.target.tagName === "LI") {
+          e.target.classList.toggle("checked");
+          saveData();
+        } else if (e.target.tagName === "SPAN") {
+          e.target.parentElement.remove();
+          saveData();
+        }
+      });
+    });
+  }
+}
+
+showList();
